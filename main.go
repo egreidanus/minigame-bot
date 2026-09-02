5c5d2ef6c8939f11e992c4c2f3d0d88144f37a89
Eelco Greidanus <eelcogreidanusnl@gmail.com>
2026-09-02 12:20:54 +0200

    Load the DISCORD_TOKEN from the .env file and if it does not exist drop a error message

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/joho/godotenv"
 )
@@ -9,5 +10,10 @@ import (
 func main() {
 	if godotenv.Load() != nil {
 		log.Fatal("Error while loading the .env file!")
+	}
+
+	discord_token := os.Getenv("DISCORD_TOKEN")
+	if discord_token == "" {
+		log.Fatal("Failed to get the DISCORD_TOKEN from the .env file!")
 	}
 }
